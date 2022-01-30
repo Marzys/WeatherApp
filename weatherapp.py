c449a48ac17e724ef9a08d601a3b10d63ed1845c
Bogdan Pevchev <bpevchev@gmail.com>
2022-01-30 05:32:06 +0100

    New Commit

---
+++ weatherapp.py
@@ -0,0 +1,23 @@
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+owm = OWM('c857186b420803192e2ab6799ca22c15')
+mgr = owm.weather_manager()
+
+place = input("Where do you want to see the weather?: ")
+
+observation = mgr.weather_at_place(place)
+w = observation.weather
+temp = w.temperature('celsius')["temp"]
+
+print("In " + place + " it is: " + w.detailed_status)
+print ("Temperature is around: " + str(temp))
+
+if temp < 10:
+    print ("Wtf nigga thats cold")
+elif temp < 20:
+    print ("Aight thats fine")
+else:
+    print ("Warm")
+
